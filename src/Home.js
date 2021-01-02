7c66ba289f1273e6670bdecaa98d15c34961e901
Javier García <javigdeveloper@gmail.com>
2021-01-01 19:15:47 -0500

    add some blogs and their css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "First blog", body: "lorem ipsum", author: "Jhon", id: 1 },
+    { title: "Second blog", body: "lorem ipsum", author: "Joe", id: 2 },
+    { title: "Third blog", body: "lorem ipsum", author: "Joseph", id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
