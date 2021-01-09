5cb8c1707250599fba9a717c8bc96b7ef1dc5304
Javier García <javigdeveloper@gmail.com>
2021-01-09 01:39:59 -0500

    add json server file

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "First blog", body: "lorem ipsum", author: "Jhon", id: 1 },
-    { title: "Second blog", body: "lorem ipsum", author: "Joe", id: 2 },
-    { title: "Third blog", body: "lorem ipsum", author: "Joseph", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+
+  const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
     console.log("Hey");
@@ -14,9 +12,37 @@ const Home = () => {
     setBlogs(newBlogs);
   };
 
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      )}
+      <h1>The name is {name}</h1>
+      <button
+        onClick={() => {
+          let names = [
+            "Jhon",
+            "Lewis",
+            "Justin",
+            "Lance",
+            "Jules",
+            "Jhonathan",
+            "Robert",
+            "Greg",
+          ];
+          setName(names[Math.floor(Math.random() * names.length)]);
+        }}
+      >
+        Change name
+      </button>
     </div>
   );
 };
