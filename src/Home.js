3eaa0b0fe589081ee392635b17289ba313c30cc1
Javier García <javigdeveloper@gmail.com>
2021-01-17 19:57:39 -0500

    detect loading and errors

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
@@ -14,14 +15,26 @@ const Home = () => {
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data for that resource.");
+        }
+        return res.json();
+      })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
       )}
