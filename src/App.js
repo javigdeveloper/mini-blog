615bbff8b997ea31ff2f3c92ebcdb6079b68ef7b
Javier García <javigdeveloper@gmail.com>
2021-01-26 17:16:35 -0500

    add delete blog, not found page, and redirect to home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 function App() {
   return (
     <Router>
@@ -16,8 +17,11 @@ function App() {
             <Route path="/details/:id">
               <BlogDetails />
             </Route>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
